ecd26e831015e4685907c04d6556e45eecd9421b
David Y. Stephenson <david@davidystephenson.com>
2019-11-11 16:36:39 +0100

    Remove swap files

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 const lobby = {
   games: [],
-  players: [] // All the players
-}
+  players: [], // All the players
+  create (name) {
+    const game = {
+      name,
+      players: [] // Players in this game
 
-function create (name) {
-  const game = {
-    name,
-    players: [] // Players in this game
+    }
+    // const game = { "name": name }
 
+    this.games.push(game)
+  },
+  join: (player, gameName) => {
+    // const gameToJoin = lobby
+    //   .games
+    //   .find(game => {
+    //     return game.name === gameName
+    //   })
+
+    console.log('this test:', this)
+
+    let gameToJoin
+
+    this
+      .games
+      .forEach(game => {
+        if (game.name === gameName) {
+          gameToJoin = game
+        }
+      })
+
+    console.log('gameToJoin test:', gameToJoin)
+
+    gameToJoin.players.push(player)
   }
-  // const game = { "name": name }
-
-  lobby.games.push(game)
 }
 
 function register (name) {
-  lobby.players.push(name)
+  this.players.push(name)
 }
 
-create('created')
+lobby.register = register
 
-register('david')
+lobby.create('created')
+
+lobby.register('david')
+
+lobby.join('david', 'created')
 
 const report = JSON.stringify(lobby, null, 2)
 
