efa26d96c41f5d086928fa7453567c8584632737
David Y. Stephenson <david@davidystephenson.com>
2019-11-12 17:54:03 +0100

    Use classes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const lobby = {
   games: [],
-  players: [], // All the players
+  users: [], // All the players
   create (name) {
     const game = {
       name,
@@ -11,42 +11,106 @@ const lobby = {
 
     this.games.push(game)
   },
-  join: (player, gameName) => {
-    // const gameToJoin = lobby
-    //   .games
-    //   .find(game => {
-    //     return game.name === gameName
-    //   })
+  join (gameName, name, password) {
+    // const users = lobby.users
+    const { users } = lobby
 
-    console.log('this test:', this)
+    const user = users.find(user => user.name === name)
+    // return user that matches test
+    // return first user that returns true
+    console.log('user test:', user)
 
-    let gameToJoin
+    const verified = user.verify(password)
 
-    this
-      .games
-      .forEach(game => {
-        if (game.name === gameName) {
-          gameToJoin = game
-        }
-      })
+    if (verified) {
+      let gameToJoin
 
-    console.log('gameToJoin test:', gameToJoin)
+      this
+        .games
+        .forEach(game => {
+          if (game.name === gameName) {
+            gameToJoin = game
+          }
+        })
 
-    gameToJoin.players.push(player)
+      const player = new Player(name, password)
+
+      gameToJoin.players.push(player)
+
+      return player
+    }
+
+    console.log('You are unverified')
   }
 }
 
-function register (name) {
-  this.players.push(name)
+class User {
+  constructor (name, password, age = 18) {
+    age = age || 18
+
+    this.name = name
+    this.password = password
+    if (age) {
+      this.age = age
+    } else {
+      this.age = 18
+    }
+  }
+
+  verify (password) {
+    if (this.age < 18) {
+      return false
+    }
+
+    return this.password === password
+  }
+}
+
+class Player extends User {
+  constructor (name, password) {
+    super(name, password) // This is the constructor of the User class because Player extends User
+
+    this.points = 0
+  }
+
+  gain (number) {
+    this.points = this.points + number
+  }
+
+  lose (number) {
+    this.points = this.points - number
+  }
+
+  reset () {
+    this.points = 0
+  }
+
+  announce () {
+    console.log(`${this.name} has ${this.points}`)
+  }
+}
+
+function register (name, password, age) {
+  const user = new User(name, password, age)
+
+  this.users.push(user)
+
+  return user
 }
 
 lobby.register = register
 
 lobby.create('created')
 
-lobby.register('david')
+const david = lobby.register('david', 'password', 75)
+console.log('password test:', david.verify('password'))
+console.log('wrong test:', david.verify('wrong'))
 
-lobby.join('david', 'created')
+const player = lobby.join('created', 'david', 'password')
+if (player) {
+  player.gain(10)
+  player.announce()
+}
 
 const report = JSON.stringify(lobby, null, 2)
 
